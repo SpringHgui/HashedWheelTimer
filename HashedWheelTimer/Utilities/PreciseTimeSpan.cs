ceaca34f5fbd4a7e208f91bdfb78bda491fdd906
Gui.H <springhgui@outlook.com>
2023-04-13 21:08:08 +0800

    ✔

--- HashedWheelTimer/Utilities/PreciseTimeSpan.cs
+++ HashedWheelTimer/Utilities/PreciseTimeSpan.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace WheelTimer
+namespace WheelTimer.Utilities
 {
     using System;
     using System.Diagnostics;
